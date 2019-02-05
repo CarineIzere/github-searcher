d953aedcf91205dfa7c5aa9e9addb645dd1f41e8
CarineIzere <carizeree@gmail.com>
2019-02-05 12:59:04 +0200

    add repositories

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -1,6 +1,7 @@
 export class Repos {
   constructor(
     public Username: string,
+    public Repository: string,
     public Location: string,
     public Email: string,
     public MemberSince: 0
