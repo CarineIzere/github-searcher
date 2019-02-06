9064b649c1c92534feb890bec4032380e956f7a7
CarineIzere <carizeree@gmail.com>
2019-02-06 09:51:38 +0200

    correct repos.spec and user.spec.ts

--- src/app/repos.spec.ts
+++ src/app/repos.spec.ts
@@ -2,6 +2,6 @@ import { Repos } from "./repos";
 
 describe("Repos", () => {
   it("should create an instance", () => {
-    expect(new Repos("", "", "", 0)).toBeTruthy();
+    expect(new Repos("", "", 0, 0, "", 0)).toBeTruthy();
   });
 });
